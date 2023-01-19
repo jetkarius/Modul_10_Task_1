4714a23786ff098549b704dd01a0de05a696069c
boroksid@yandex.ru <boroksid@yandex.ru>
2023-01-20 00:23:48 +0300

    Добавьте файлы проекта.

---
+++ Modul_10_Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Modul_10_Task_1
+{
+    public interface ICalculator
+    {
+        void Sum(int a, int b);
+    }
+
+    public class MyCalc : ICalculator
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine("Ответ: {0} ", a + b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator mycalc;
+            try
+            {
+                Console.WriteLine("Значение 1: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Значение 2: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                mycalc = new MyCalc();
+                mycalc.Sum(a, b);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Не корректное значение!");
+            }
+            finally
+            {
+                Console.WriteLine("Расчёт окончен");
+            }
+        }
+    }
+}
